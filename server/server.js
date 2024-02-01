46f3b7e3c07d54df10dcf1779acd390cdfdd537c
Ashutosh Raj Gupta <ag2364443@gmail.com>
2024-02-01 22:45:19 +0530

    sign up issue bug fixed

--- server/server.js
+++ server/server.js
@@ -1,42 +1,42 @@
-const express =  require("express");
-const mysql = require('mysql');
-const cors=require('cors');
+// app.use(express.json());
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "signup",
+// });
 
-const app =express();
-app.use(cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const app = express();
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
-const db= mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "",
-    database : "signup"
 
-})
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "ashutosh",
+  database: "evee",
+});
 
-app.post('/signup', (req, res) => {
-    const sql= "INSERT INTO login('username','password') VALUES (?)";
-    const values=[
-        req.body.username,
-        req.body.password
-    ]
-    db.query(sql, [values],(err,data) => {
-        if(err)
-        {
-            return res.json("error");
-        }
-        return res.json(data);
-    })
-})
+app.post("/signup", (req, res) => {
+  const { username, password } = req.body;
 
-app.listen(8081 , () =>
-{
-    console.log("listening");
-})
+  // Validate inputs here if needed
 
+  const sql = "INSERT INTO login (username, password) VALUES (?, ?)";
+  const values = [username, password];
+
+  db.query(sql, values, (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+    return res.status(201).json({ message: "User created successfully" });
+  });
+});
+
+app.listen(8081, () => {
+  console.log("Server is running on port 8081");
+});
